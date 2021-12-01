7c30afcea5e544f833d5fc5a9cdfaff72a4f9425
Jamie Brynes <jamiebrynes7@gmail.com>
2021-12-01 14:35:54 +0000

    with const generics

--- src/day01/main.rs
+++ src/day01/main.rs
@@ -8,8 +8,8 @@ const PATH: &str = "src/day01/input.txt";
 
 fn main() -> anyhow::Result<()> {
     let input = get_input()?;
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    println!("Part 1: {}", calc::<1>(&input));
+    println!("Part 2: {}", calc::<3>(&input));
 
     Ok(())
 }
@@ -24,26 +24,11 @@ fn get_input() -> anyhow::Result<Vec<u64>> {
         .collect::<Result<Vec<u64>, _>>()?)
 }
 
-fn part1(data: &Vec<u64>) -> u32 {
+fn calc<const T: usize>(data: &Vec<u64>) -> u32 {
     let mut count = 0;
-    let mut last = *data.get(0).unwrap();
+    let mut last: u64 = data[0..T].iter().sum();
 
-    for current in &data[1..] {
-        if *current > last {
-            count += 1;
-        }
-
-        last = *current;
-    }
-
-    count
-}
-
-fn part2(data: &Vec<u64>) -> u32 {
-    let mut count = 0;
-    let mut last: u64 = data[0..3].iter().sum();
-
-    for current in data[1..].windows(3) {
+    for current in data[1..].windows(T) {
         let sum: u64 = current.iter().sum();
 
         if sum > last {
